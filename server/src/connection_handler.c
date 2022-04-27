640b087902417528e71a2f6c37c3d9462f742a60
Ger0r0r <fedor.bombax@gmail.com>
2022-04-27 21:36:04 +0300

    bug fix

--- server/src/connection_handler.c
+++ server/src/connection_handler.c
@@ -53,6 +53,14 @@ void Send_admin_info(int sigN, siginfo_t* sigInfo, void* context){
 void New_admin_request(int sigN, siginfo_t* sigInfo, void* context){
 	if (sigN != SIGUSR2)
 		return;
+
+	printf("Info about connection:\n");
+	printf("Client - %s:%d\n", inet_ntoa(bfd->client.sin_addr), htons(bfd->client.sin_port));
+	printf("Keys - %s %s\n", bfd->key, bfd->iv);
+	printf("Database\n");
+	for (size_t i = 0; i < bfd->c_users; i++){
+		printf("%s %s %s %s\n", bfd->users[i]->login, bfd->users[i]->password, bfd->users[i]->key_old, bfd->users[i]->IV_old);
+	}printf("\n");
 	
 	int pid = fork();
 
